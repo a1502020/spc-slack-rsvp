f9707746744a4d9320dc8c0d43c2ebf77585d876
Chisaka <a1502020@sendai-nct.jp>
2016-04-03 04:14:45 +0900

    Slack の RTM に接続してメッセージを受信できるようにした

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'slack-ruby-client'
+
+Slack.configure do |conf|
+  conf.token = File.open('slack-token').read.chomp
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :hello do
+  puts 'connected.'
+end
+
+client.on :message do |data|
+  puts data.to_json
+end
+
+client.start!
