950fa1a60d61f4cff8c75740dd501347987ffe4e
Chisaka <a1502020@sendai-nct.jp>
2016-04-04 04:02:31 +0900

    ログに出力する内容を見やすくした

--- app.rb
+++ app.rb
@@ -12,16 +12,18 @@ Slack.configure do |conf|
   conf.token = File.open('slack-token').read.chomp
 end
 
+def format_log_message(data)
+  "#{data['channel']} #{data['user']} \"#{data['text'].gsub(/\\/, '\\\\\\\\').gsub(/\r\n|\r|\n/, '\\n')}\""
+end
+
 client = Slack::RealTime::Client.new(logger: Logger.new('log/client.log'))
 
 client.on :hello do
-  log.info(':hello')
   puts 'connected.'
 end
 
 client.on :message do |data|
-  log.info(':message')
-  log.info(data.to_json)
+  log.info(format_log_message(data))
   if $words_rsvp.include?(data['text'])
     client.message channel: data['channel'], text:'未実装だよ。ちょっと待っててね。'
   else
