ac95ad640f373f73d00324fee2244af39898d450
Chisaka <a1502020@sendai-nct.jp>
2016-04-03 23:37:46 +0900

    イベントでログを記録するようにした

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'slack-ruby-client'
+require 'logger'
 require './db_slack_rsvp'
 require './words'
+
+log = Logger.new('slack_rsvp.log')
 
 db = DBSlackRsvp.new
 
@@ -11,10 +14,13 @@ end
 client = Slack::RealTime::Client.new
 
 client.on :hello do
+  log.info(':hello')
   puts 'connected.'
 end
 
 client.on :message do |data|
+  log.info(':message')
+  log.info(data.to_json)
   if $words_rsvp.include?(data['text'])
     client.message channel: data['channel'], text:'未実装だよ。ちょっと待っててね。'
   else
